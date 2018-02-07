de4a205c0fda83bf673519fadf7a242ec78a7256
coolsnowwolf <coolsnowwolf@gmail.com>
2018-02-07 17:42:31 +0800

    fix dependency

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -12,7 +12,7 @@ try_version() {
 }
 
 try_git() {
-	REBOOT=ee53a240ac902dc83209008a2671e7fdcf55957a
+	REBOOT=2f552062794460b823f90e6cbf708ebdf6da9901
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
 
 	[ -n "$GET_REV" ] || GET_REV="HEAD"
